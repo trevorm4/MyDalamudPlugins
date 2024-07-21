8ff012b085bfa462a858669ea95b6fce4039ea3d
trevorm4 <trevorm4@illinois.edu>
2024-07-20 22:01:17 -0700

    update

--- generate_pluginmaster.py
+++ generate_pluginmaster.py
@@ -3,9 +3,10 @@ import os
 from time import time
 from sys import argv
 from os.path import getmtime
-from zipfile import ZipFile, ZIP_DEFLATED
+from zipfile import ZipFile
+from argparse import ArgumentParser
 
-DOWNLOAD_URL = 'https://github.com/trevorm4/MyDalamudPlugins/raw/master/plugins/{plugin_name}/latest.zip'
+DOWNLOAD_URL = 'https://github.com/{repo}/raw/main/plugins/{plugin_name}/latest.zip'
 
 DEFAULTS = {
     'IsHide': False,
@@ -30,6 +31,10 @@ TRIMMED_KEYS = [
 ]
 
 def main():
+    parser = ArgumentParser("create_json")
+    parser.add_argument("--repo", "-repo", default="trevorm4/MyDalamudPlugins")
+    parser.add_argument("--file_name", "-f", help="File to read/write to", default="repo.json")
+    args = parser.parse_args()
     # extract the manifests from inside the zip files
     master = extract_manifests()
 
@@ -37,13 +42,13 @@ def main():
     master = [trim_manifest(manifest) for manifest in master]
 
     # convert the list of manifests into a master list
-    add_extra_fields(master)
+    add_extra_fields(args.repo, master)
 
     # write the master
-    write_master(master)
+    write_master(args.file_name, master)
 
     # update the LastUpdated field in master
-    last_updated()
+    last_updated(args.file_name)
 
 def extract_manifests():
     manifests = []
@@ -59,10 +64,10 @@ def extract_manifests():
 
     return manifests
 
-def add_extra_fields(manifests):
+def add_extra_fields(repo_name, manifests):
     for manifest in manifests:
         # generate the download link from the internal assembly name
-        manifest['DownloadLinkInstall'] = DOWNLOAD_URL.format(plugin_name=manifest["InternalName"])
+        manifest['DownloadLinkInstall'] = DOWNLOAD_URL.format(repo=repo_name, plugin_name=manifest["InternalName"])
         # add default values if missing
         for k, v in DEFAULTS.items():
             if k not in manifest:
@@ -74,16 +79,16 @@ def add_extra_fields(manifests):
                     manifest[k] = manifest[source]
         manifest['DownloadCount'] = 0
 
-def write_master(master):
+def write_master(file_name, master):
     # write as pretty json
-    with open('pluginmaster.json', 'w') as f:
+    with open(file_name, 'w') as f:
         json.dump(master, f, indent=4)
 
 def trim_manifest(plugin):
     return {k: plugin[k] for k in TRIMMED_KEYS if k in plugin}
 
-def last_updated():
-    with open('pluginmaster.json') as f:
+def last_updated(file_name):
+    with open(file_name) as f:
         master = json.load(f)
 
     for plugin in master:
@@ -93,7 +98,7 @@ def last_updated():
         if 'LastUpdated' not in plugin or modified != int(plugin['LastUpdated']):
             plugin['LastUpdated'] = str(modified)
 
-    with open('pluginmaster.json', 'w') as f:
+    with open(file_name, 'w') as f:
         json.dump(master, f, indent=4)
 
 if __name__ == '__main__':
